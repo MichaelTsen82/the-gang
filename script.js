bb50085d563e02edb00d31850a9c942f08b15158
Tsen Joon Onn <tsenjoononn@gmail.com>
2018-05-02 16:54:51 +0800

    final css updated

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 console.log('hey there!');
 
 function showPopup () {
-	var popup = document.getElementById('popup');
-	popup.classList.add('show');
+	console.log('APPEAR');
+	var popup = document.getElementById("popup");
+	popup.classList.add("show");
 }
 
 function hidePopup () {
-	var popup = document.getElementById('popup');
-	popup.classList.remove('show');
+	console.log('BYE');
+	var popup = document.getElementById("popup");
+	popup.classList.remove("show");
 }
